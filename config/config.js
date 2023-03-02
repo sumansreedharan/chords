d215b9da76a0ebad8d3fb1b376b5a766081ab2b2
suman <sumankannath@gmail.com>
2023-03-02 13:50:42 +0530

    chan

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ const emailPassword = "askrtcywkrfquxls"
 const DB = ()=>{
     const mongoose = require("mongoose")
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env. databaseUrl+process.env.database)
+mongoose.connect(process.env.databaseUrl+process.env.database)
 }
 
 
